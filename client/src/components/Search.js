c65b4a761d91dee96c506ba4bd2a64249f993680
Alan Nguyen <nguyenalan@berkeley.edu>
2019-12-08 14:24:09 -0800

    modify server-side, added extra GET endpoint

---
+++ client/src/components/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
+
+const SearchContainer = () => {
+    
+}
+
+export default SearchContainer;
--- client/src/containers/Cart.js
+++ client/src/containers/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
+import { CartActions } from '../actions';
 import { Items } from '../components';
 
 const Wrapper = styled.div``;
 
 const CartContainer = () => {
     const currState = useSelector((state) => state.cartReducer);
+    const dispatch = useDispatch();
     return (
         <Wrapper>
 
