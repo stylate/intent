b5eff0c80c3416cd56b276983942e4c6f06a0d10
Alan Nguyen <nguyenalan@berkeley.edu>
2019-12-08 12:44:23 -0800

    implement redux persistor + store

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import Store from './reducers/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Store, Persistor } from './reducers/store';
 import 'semantic-ui-less/semantic.less';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <Provider store={Store}>
-        <App />
-    </Provider>, document.getElementById('root'),
+const rend = () => {
+    return (
+        <Provider store={Store}>
+            <PersistGate loading={null} persistor={Persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    )
+};
+
+ReactDOM.render(rend(), document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
--- client/src/reducers/store.js
+++ client/src/reducers/store.js
@@ -1,7 +1,16 @@
-  import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 import rootReducer from './index';
 
-const Store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default Store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const Store = createStore(persistedReducer, applyMiddleware(thunk));
+export const Persistor = persistStore(Store);
